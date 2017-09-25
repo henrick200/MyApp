471ea1e515e3c92ab77e173e6547845bf3652e15
ASUS <henrick20132013@gmail.com>
2017-09-25 20:38:50 -0300

    Funcionando pela METADE

--- app/src/main/java/com/example/aluno/myapp/MainActivity.java
+++ app/src/main/java/com/example/aluno/myapp/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.aluno.myapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    TextView t1;
-    MyLocation.LocationResult locationResult;
+    private Button btn;
+    private TextView t1;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +31,61 @@ public class MainActivity extends AppCompatActivity {
 
         btn = (Button) findViewById(R.id.button);
         t1 = (TextView) findViewById(R.id.tt);
-         locationResult = new MyLocation.LocationResult() {
-             @Override
-             public void gotLocation(Location location) {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                t1.setText("\n" + location.getLatitude() + "" + location.getAltitude());
+            }
 
-             }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
-         };
-        MyLocation myLocation = new MyLocation();
-        myLocation.getLocation(this, locationResult);
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
+        }else{
+            configureButton();
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+        }
+    }
+
+    private void configureButton() {
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
             }
         });
 
