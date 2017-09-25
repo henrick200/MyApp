fbbe699619d846a6c196fd372e77101e2642c6a3
Aluno <henrick20132013@gmail.com>
2017-09-25 09:05:35 -0300

    criando MyLocation

--- app/src/main/java/com/example/aluno/myapp/MainActivity.java
+++ app/src/main/java/com/example/aluno/myapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.aluno.myapp;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView t1;
+    MyLocation.LocationResult locationResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        t1 = (TextView) findViewById(R.id.tt);
+         locationResult = new MyLocation.LocationResult() {
+             @Override
+             public void gotLocation(Location location) {
+
+             }
+
+         };
+        MyLocation myLocation = new MyLocation();
+        myLocation.getLocation(this, locationResult);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
 }
